9481b4c8877d6683306cde4e7b87c90072a2f9d3
ian ayiro <ian.ayiro@student.moringa.com>
2024-07-20 09:18:09 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write her your code
+
+//helper.js
+const numbers = Math.floor(Math.random() * 60) + 2;
+    const num1 = 6;
+    const num2 = 10;
+    const multiply = num1 * num2; 
+    const random = Math.floor(Math.random() * 100) + 1; //generating an interger between 1 and 100
+const num3 = 14;
+const num4 = 10 ;
+const mod = num3 % num4;
+const max =  Math.max(1, 5, 10, 20,);
+
+module .exports = {num1, num2, multiply, random, num3, num4, mob, max };
